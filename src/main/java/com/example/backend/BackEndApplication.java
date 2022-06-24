b3a2a13cc5b01fb3350bc8893e193bac40560fbb
geonoo <yung65322136@gmail.com>
2022-06-24 12:56:52 +0900

    테스트 파일 추가
    추가추가

--- src/main/java/com/example/backend/BackEndApplication.java
+++ src/main/java/com/example/backend/BackEndApplication.java
@@ -2,8 +2,11 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({ @PropertySource("classpath:application-aws.properties") })
 public class BackEndApplication {
 
     public static void main(String[] args) {
