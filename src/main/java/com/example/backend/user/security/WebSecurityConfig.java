a337ee96cb3ac818e871ee01db10d2572a379bc9
geonoo <yung65322136@gmail.com>
2022-07-01 23:04:46 +0900

    #11 로그인 기능 Cors 설정
    cors header 설정

--- src/main/java/com/example/backend/user/security/WebSecurityConfig.java
+++ src/main/java/com/example/backend/user/security/WebSecurityConfig.java
@@ -34,16 +34,17 @@ public class WebSecurityConfig {
 //        return super.authenticationManagerBean();
 //    }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT","DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOrigin("http://localhost:3000");
+//        configuration.addAllowedOrigin("*");
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT","DELETE"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     //아래 형식의 URL로 들어오는 요청에 대해 인증을 요구
     @Bean
@@ -51,7 +52,7 @@ public class WebSecurityConfig {
         http
             .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
             .csrf().disable() // csrf 보안 토큰 disable처리.
-            .cors().configurationSource(corsConfigurationSource()).and()
+//            .cors().configurationSource(corsConfigurationSource()).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
             .and()
             .authorizeRequests() // 요청에 대한 사용권한 체크
