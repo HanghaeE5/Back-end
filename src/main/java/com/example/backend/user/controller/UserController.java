7411d6960af036464e197ab39eb11a2a08757d58
geonoo <yung65322136@gmail.com>
2022-07-06 11:56:25 +0900

    #28 사용자 기능
    1. 프로필 사진 변경
    2. 닉네임 변경

--- src/main/java/com/example/backend/user/controller/UserController.java
+++ src/main/java/com/example/backend/user/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.backend.user.controller;
 
 import com.example.backend.user.common.LoadUser;
+import com.example.backend.user.dto.NickRequestDto;
 import com.example.backend.user.dto.UserResponseDto;
 import com.example.backend.user.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -18,10 +21,29 @@ public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "사용자 정보 조회")
     @GetMapping
     public ResponseEntity<UserResponseDto> getUser() {
         return ResponseEntity
                 .ok()
-                .body(userService.getUser(LoadUser.getEmail()));
+                .body(userService.getUserInfo(LoadUser.getEmail()));
+    }
+
+    @ApiOperation(value = "사용자 프로필 사진 변경")
+    @PutMapping("/profile")
+    public ResponseEntity<UserResponseDto> updateProfile(@RequestPart(value = "file") MultipartFile file){
+        LoadUser.loginAndNickCheck();
+        return ResponseEntity
+                .ok()
+                .body(userService.updateProfile(file, LoadUser.getEmail()));
+    }
+
+    @ApiOperation(value = "사용자 닉네임 변경")
+    @PutMapping("/nick")
+    public ResponseEntity<UserResponseDto> updateNick(@Valid @RequestBody NickRequestDto nickRequestDto){
+        LoadUser.loginAndNickCheck();
+        return ResponseEntity
+                .ok()
+                .body(userService.updateNick(nickRequestDto.getNick(), LoadUser.getEmail()));
     }
 }
