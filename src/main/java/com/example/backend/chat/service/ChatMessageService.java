4da93387e9652350a6edea5db7761016f1e8b5d4
geonoo <yung65322136@gmail.com>
2022-07-12 02:59:20 +0900

    #22 사라졌던 파일들
    사라졌던 파일들

--- src/main/java/com/example/backend/chat/service/ChatMessageService.java
+++ src/main/java/com/example/backend/chat/service/ChatMessageService.java
@@ -1,8 +1,12 @@
 package com.example.backend.chat.service;
 
-import com.example.backend.chat.dto.request.ChatMessageRequestDto;
+import com.example.backend.chat.domain.ChatMessage;
 import com.example.backend.chat.domain.ChatRoom;
+import com.example.backend.chat.domain.MessageType;
 import com.example.backend.chat.domain.Participant;
+import com.example.backend.chat.dto.request.ChatMessageRequestDto;
+import com.example.backend.chat.dto.response.ChatMessageResponseDto;
+import com.example.backend.chat.repository.ChatMessageRepository;
 import com.example.backend.chat.repository.ChatRoomRepository;
 import com.example.backend.chat.repository.ParticipantRepository;
 import com.example.backend.exception.CustomException;
@@ -11,6 +15,10 @@ import com.example.backend.user.domain.User;
 import com.example.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +35,6 @@ public class ChatMessageService {
     private final ChatRoomRepository chatRoomRepository;
     private final UserRepository userRepository;
     private final ChatMessageRepository chatMessageRepository;
-
 
     @Transactional
     public void addParticipant(String email, String roomId) {
@@ -61,19 +68,29 @@ public class ChatMessageService {
 
 
     public void sendChatMessage(ChatMessageRequestDto message) {
-        if (ChatMessageRequestDto.MessageType.ENTER.equals(message.getType())
-                || ChatMessageRequestDto.MessageType.QUIT.equals(message.getType())) {
+        if (MessageType.ENTER.equals(message.getType())
+                || MessageType.QUIT.equals(message.getType())) {
             message.setSender("[알림]");
         }
 
         log.info("getMessage : " + message.getMessage());
         log.info("message.getRoomId() : " + message.getRoomId());
         log.info("getSender : " + message.getSender());
-        System.out.println(message.toString());
+        System.out.println(message);
 
         this.saveChatMessage(message);
-
         messageSendingOperations.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
+    // 페이징으로 받아서 무한 스크롤 가능할듯
+    public Page<ChatMessageResponseDto> getSavedMessages(String roomId) {
+        Pageable pageable = PageRequest.of(0, 100, Sort.by("createdDate").descending());
+
+        Page<ChatMessage> messagePage = chatMessageRepository.findAllByRoomId(pageable, roomId);
+        return messagePage.map(ChatMessageResponseDto::new);
+    }
+
+    public void saveChatMessage(ChatMessageRequestDto message) {
+        chatMessageRepository.save(new ChatMessage(message));
+    }
 }
