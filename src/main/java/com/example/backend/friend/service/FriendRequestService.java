0c21d410e88d8903bebf90e05aa5ef7e6a7eca2c
geonoo <yung65322136@gmail.com>
2022-08-03 07:11:33 +0900

    #153 알림 메시지 전송2
    알림 메시지 전송2

--- src/main/java/com/example/backend/friend/service/FriendRequestService.java
+++ src/main/java/com/example/backend/friend/service/FriendRequestService.java
@@ -78,7 +78,7 @@ public class FriendRequestService {
         NotificationRequestDto notificationRequestDto = new NotificationRequestDto(
                 Type.FRIEND_REQUEST,
                 user.getUsername()+"님이 "+userTo.getUsername()+"님께 친구 신청을 하셨습니다!"
-                +"<br/>친구 페이지로 이동하셔서 수락해주세요."
+                +"<br/>친구 페이지로 이동하셔서 수락 또는 거절 하실 수 있어요!"
         );
 
         notificationService.sendNotification(userTo.getUserSeq(), notificationRequestDto);
