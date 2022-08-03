aefe955a61eacf310f1d6ae1f12497c3b96de2df
geonoo <yung65322136@gmail.com>
2022-08-03 10:03:07 +0900

    #153 알림 리스트
    알림 리스트

--- src/main/java/com/example/backend/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/backend/notification/repository/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.example.backend.notification.repository;
 
 import com.example.backend.notification.domain.Notification;
+import com.example.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUser(User user);
 }
